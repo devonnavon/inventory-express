bd1371c3af1e6fcb7c38885b0ca5ae21eb9e9fa9
Devon Navon <devon.navon@gmail.com>
2020-06-27 12:17:03 -0700

    skeleton route controllers implemented

---
+++ controllers/productController.js
@@ -0,0 +1,33 @@
+var Product = require('../models/product');
+
+exports.product_list = (req, res) => {
+	res.send('Not yet! product list');
+};
+
+exports.product_detail = (req, res) => {
+	res.send('Not yet! product detail' + req.params.id);
+};
+
+exports.product_create_get = (req, res) => {
+	res.send('Not yet! product create get');
+};
+
+exports.product_create_post = (req, res) => {
+	res.send('Not yet! product create post');
+};
+
+exports.product_delete_get = (req, res) => {
+	res.send('Not yet! product delete get');
+};
+
+exports.product_delete_post = (req, res) => {
+	res.send('Not yet! product delete post');
+};
+
+exports.product_update_get = (req, res) => {
+	res.send('Not yet! product update get');
+};
+
+exports.product_update_post = (req, res) => {
+	res.send('Not yet! product update post');
+};
