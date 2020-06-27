50c9f055a8e8f5692ad3f573e8a0553ab2e7fab4
Devon Navon <devon.navon@gmail.com>
2020-06-27 12:16:06 -0700

    skeleton route controllers implemented

---
+++ controllers/brandController.js
@@ -0,0 +1,33 @@
+var Brand = require('../models/brand');
+
+exports.brand_list = (req, res) => {
+	res.send('Not yet! brand list');
+};
+
+exports.brand_detail = (req, res) => {
+	res.send('Not yet! brand detail' + req.params.id);
+};
+
+exports.brand_create_get = (req, res) => {
+	res.send('Not yet! brand create get');
+};
+
+exports.brand_create_post = (req, res) => {
+	res.send('Not yet! brand create post');
+};
+
+exports.brand_delete_get = (req, res) => {
+	res.send('Not yet! brand delete get');
+};
+
+exports.brand_delete_post = (req, res) => {
+	res.send('Not yet! brand delete post');
+};
+
+exports.brand_update_get = (req, res) => {
+	res.send('Not yet! brand update get');
+};
+
+exports.brand_update_post = (req, res) => {
+	res.send('Not yet! brand update post');
+};
---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+var Category = require('../models/category');
+
+exports.category_list = (req, res) => {
+	res.send('Not yet! category list');
+};
+
+exports.category_detail = (req, res) => {
+	res.send('Not yet! category detail' + req.params.id);
+};
+
+exports.category_create_get = (req, res) => {
+	res.send('Not yet! category create get');
+};
+
+exports.category_create_post = (req, res) => {
+	res.send('Not yet! category create post');
+};
+
+exports.category_delete_get = (req, res) => {
+	res.send('Not yet! category delete get');
+};
+
+exports.category_delete_post = (req, res) => {
+	res.send('Not yet! category delete post');
+};
+
+exports.category_update_get = (req, res) => {
+	res.send('Not yet! category update get');
+};
+
+exports.category_update_post = (req, res) => {
+	res.send('Not yet! category update post');
+};
